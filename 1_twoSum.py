5cca15bdf3cc760fab5e5b7cf7ade4f9c8a10e54
shrinkdo <shrink.du@gmail.com>
2017-11-24 23:50:18 -0600

    added twoSum sol

---
+++ 1_twoSum.py
@@ -0,0 +1,22 @@
+
+def twoSum(nums, target):
+    diff_cache = {}
+
+    for i in range(len(nums)):
+        x = nums[i]
+        if x in diff_cache: return sorted([diff_cache[x], i])
+        else: diff_cache[target - x] = i
+
+    assert 0, "no solution"
+
+if __name__ == "__main__":
+    nums   = [2, 7, 11, 15]
+    target = 9
+    sol    = twoSum(nums, target)
+    print(sol)
+    assert sol == [0, 1]
+
+    target = 17
+    sol    = twoSum(nums, target)
+    print(sol)
+    assert sol == [0, 3]
